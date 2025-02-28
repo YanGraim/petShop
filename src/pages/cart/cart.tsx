83304306ff7e2c11e650c096e80927abb7caf432
YanGraim <yangraim@gmail.com>
2025-02-28 10:29:29 -0300

    adiciona pasta de pages

---
+++ src/pages/cart/cart.tsx
@@ -0,0 +1,7 @@
+export function Cart() {
+    return (
+        <div>
+            <h1>Page cart</h1>
+        </div>
+    )
+}
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return (
+        <div>
+            <h1>Page home</h1>
+        </div>
+    )
+}
