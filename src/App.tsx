9b7afa3f0494be8959da1d364a44a63a539fcd9f
YanGraim <yangraim@gmail.com>
2025-03-08 12:59:27 -0300

    cria rota para pagina de detalhes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/home/home";
 import { Cart } from "./pages/cart/cart";
 import { Layout } from "./components/layout/layout";
+import { Details } from "./pages/detail/detail";
 
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         path: "/cart",
         element: <Cart/>
+      },
+      {
+        path: "/product/:id",
+        element: <Details/>
       }
     ]
   }
